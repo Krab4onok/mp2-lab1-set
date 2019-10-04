1c6802f0dd2882baa475a69a21bc4c594d91fce0
VladdFonotov <seriakov.andrej@yandex.ru>
2019-10-04 11:36:56 +0300

    First commit

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -22,29 +22,65 @@ private:
   int  MemLen; // к-во эл-тов Мем для представления бит.поля
 
   // методы реализации
-  int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
-  TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
+  int   GetMemIndex(const int n) const { return n / 32; }; // индекс в pМем для бита n       (#О2)
+  TELEM GetMemMask(const int n) const {
+	  int v;
+	  v = n % 32;
+	  TELEM tmp = 1;
+	  tmp = tmp << v;
+	  return tmp;
+  }; // битовая маска для бита n       (#О3)
 public:
-  TBitField(int len);                //                                   (#О1)
-  TBitField(const TBitField &bf);    //                                   (#П1)
-  ~TBitField();                      //                                    (#С)
+	TBitField(int len = 15) 
+	{
+		BitLen = len;
+		MemLen = ((len) / 32) + 1;
+		pMem = new TELEM[MemLen];
+		for (int i = 0; i < MemLen; i++) 
+		{
+			pMem[i] = 0;
+		}
+
+	};                //                                   (#О1)
+	TBitField(const TBitField& a) {
+		BitLen = a.BitLen;
+		MemLen = a.MemLen;
+		pMem = new TELEM[MemLen];
+		for (int i = 0; i < MemLen; i++)
+		{
+			pMem[i] = a.pMem[i];
+		}
+	};    //                                   (#П1)
+	~TBitField() {
+		delete[] pMem;
+	};                      //                                    (#С)
 
   // доступ к битам
-  int GetLength(void) const;      // получить длину (к-во битов)           (#О)
-  void SetBit(const int n);       // установить бит                       (#О4)
-  void ClrBit(const int n);       // очистить бит                         (#П2)
-  int  GetBit(const int n) const; // получить значение бита               (#Л1)
+	int GetLength(void) const { return BitLen; };      // получить длину (к-во битов)           (#О)
+	void SetBit(const int n) {};       // установить бит                       (#О4)
+	void ClrBit(const int n) {};       // очистить бит                         (#П2)
+	int  GetBit(const int n) const {}; // получить значение бита               (#Л1)
 
   // битовые операции
-  int operator==(const TBitField &bf) const; // сравнение                 (#О5)
-  int operator!=(const TBitField &bf) const; // сравнение
-  TBitField& operator=(const TBitField &bf); // присваивание              (#П3)
-  TBitField  operator|(const TBitField &bf); // операция "или"            (#О6)
-  TBitField  operator&(const TBitField &bf); // операция "и"              (#Л2)
-  TBitField  operator~(void);                // отрицание                  (#С)
+	int operator==(const TBitField& bf) const {}; // сравнение                 (#О5)
+	int operator!=(const TBitField& bf) const {}; // сравнение
+	TBitField& operator=(const TBitField& a)
+	{
+		BitLen = a.BitLen;
+		MemLen = a.MemLen;
+		pMem = new TELEM[MemLen];
+		for (int i = 0; i < MemLen; i++)
+		{
+			pMem[i] = a.pMem[i];
+		}
 
-  friend istream &operator>>(istream &istr, TBitField &bf);       //      (#О7)
-  friend ostream &operator<<(ostream &ostr, const TBitField &bf); //      (#П4)
+	}; // присваивание              (#П3)
+	TBitField  operator|(const TBitField& bf) {}; // операция "или"            (#О6)
+	TBitField  operator&(const TBitField& bf) {}; // операция "и"              (#Л2)
+	TBitField  operator~(void) {};                // отрицание                  (#С)
+
+  friend istream& operator>>(istream& istr, TBitField& bf) {};       //      (#О7)
+  friend ostream& operator<<(ostream& ostr, const TBitField& bf) {}; //      (#П4)
 };
 // Структура хранения битового поля
 //   бит.поле - набор битов с номерами от 0 до BitLen
