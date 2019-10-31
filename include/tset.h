3ef213a9f1ab0cd7293eaeebffd126bd845bdb44
Krab4onok <seriakov.andrej@yandex.ru>
2019-10-31 18:07:05 +0300

    Tset without tests

--- include/tset.h
+++ include/tset.h
@@ -13,43 +13,104 @@
 class TSet
 {
 private:
-  int MaxPower;       // максимальная мощность множества
-  TBitField BitField; // битовое поле для хранения характеристического вектора
+	int MaxPower;       // максимальная мощность множества
+	TBitField BitField; // битовое поле для хранения характеристического вектора
 public:
 	TSet(int mp) :BitField(mp)
 	{
 		MaxPower = mp;
 	};
 	TSet(const TSet& s) :BitField(s.BitField), MaxPower(s.MaxPower) {};       // конструктор копирования
-  TSet(const TBitField &bf) :BitField(s.BitField), MaxPower(s.MaxPower) {}; // конструктор преобразования типа
-  operator TBitField()
-  {
-	  return BitField;
-  };      // преобразование типа к битовому полю
-  // доступ к битам
-  int GetMaxPower(void) const;     // максимальная мощность множества
-  void InsElem(const int Elem);       // включить элемент в множество
-  void DelElem(const int Elem);       // удалить элемент из множества
-  int IsMember(const int a) const
-  {
-	  return BitField.GetBit(a);
-  }; // проверить наличие элемента в множестве
-  // теоретико-множественные операции
-  int operator== (const TSet &s) const; // сравнение
-  int operator!= (const TSet &s) const; // сравнение
-  TSet& operator=(const TSet &s);  // присваивание
-  TSet operator+ (const int Elem); // объединение с элементом
-                                   // элемент должен быть из того же универса
-  TSet operator- (const int Elem); // разность с элементом
-                                   // элемент должен быть из того же универса
-  TSet operator+ (const TSet& s)
-  {
-	  TBitField temp;
-  };  // объединение
-  TSet operator* (const TSet &s);  // пересечение
-  TSet operator~ (void);           // дополнение
+	TSet(const TBitField &s) :BitField(s), MaxPower(s.GetLength()) {}; // конструктор преобразования типа
+	operator TBitField()
+	{
+		return BitField;
+	};      // преобразование типа к битовому полю
+	// доступ к битам
+	int GetMaxPower(void) const
+	{
+		return MaxPower;
+	};     // максимальная мощность множества
+	void InsElem(const int &a)
+	{
+		BitField.SetBit(a);
+	};       // включить элемент в множество
+	void DelElem(const int &a)
+	{
+		BitField.ClrBit(a);
+	};       // удалить элемент из множества
+	int IsMember(const int &a)const
+	{
+		return BitField.GetBit(a);
+	}; // проверить наличие элемента в множестве
+	// теоретико-множественные операции
+	bool operator== (const TSet &s) const
+	{
+		return BitField == s.BitField;
+	}; // сравнение
+	bool operator!= (const TSet &s) const
+	{
+		return BitField != s.BitField;
+	}; // сравнение
+	TSet& operator=(const TSet &s)
+	{
+		MaxPower = s.MaxPower;
+		BitField = s.BitField;
+		return *this;
+	};  // присваивание
+	TSet operator+ (const int &a)
+	{
+		TSet res(*this);
+		res.InsElem(a);
+		return res;
+	}; // объединение с элементом
+									 // элемент должен быть из того же универса
+	TSet operator- (const int &a) // разность с элементом
+	{
+		TSet res(*this);
+		res.DelElem(a);
+		return res;
+	};                        // элемент должен быть из того же универса
+	TSet operator+ (const TSet& a)
+	{
+		TBitField temp;
+		temp = BitField | a.BitField;
+		TSet res(temp);
+		return res;
 
-  friend istream &operator>>(istream &istr, TSet &bf);
-  friend ostream &operator<<(ostream &ostr, const TSet &bf);
+	};  // объединение
+	TSet operator* (const TSet &s)
+	{
+		TSet res(*this);
+		res.BitField&s.BitField;
+		return res;
+	};  // пересечение
+	TSet operator~ ()
+	{
+		return ~BitField;
+	};           // дополнение
+
+	friend istream &operator>>(istream &is, TSet &a)
+	{
+		int i;
+		is >> i;
+		while (i >= 0 || i < a.MaxPower)
+		{
+			a.InsElem(i);
+			is >> i;
+		}
+		return is;
+	};
+	friend ostream &operator<<(ostream &os, const TSet &a)
+	{
+		for (int i = 0; i < a.MaxPower; i++)
+		{
+			if(a.BitField.GetBit(i))
+			{
+				os << i << " ";
+			}
+		}
+		return os;
+	};
 };
 #endif
